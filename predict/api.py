c1196d73b44c98b6c8e95c33d12cfc76538f0497
Stefan Heinecke <stefan.heinecke1@gmail.com>
2025-07-08 13:19:37 +0200

    add history

--- predict/api.py
+++ predict/api.py
@@ -1,8 +1,15 @@
+import data
 from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
 @app.get("/sum")
 def sum_numbers(a: float = Query(...), b: float = Query(...)):
     result = a + b
     return JSONResponse(content={"sum": result})
+    
+@app.get("/history")
+def get_history():
+    history_data = data.load_data()
+    return JSONResponse(content={"history": history_data})
